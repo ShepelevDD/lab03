587ff26795894b8dc3a60fa675cb4e1c692d1687
Dima (ShepelevDD) <shepelev.mit@yandex.ru>
2020-04-10 19:20:25 +0300

    lab03cpp

---
+++ lab03.cpp
@@ -0,0 +1,46 @@
+#include "lab03.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void find_minmax(vector<double> numbers, double& min, double& max)
+    {
+      min = numbers[0];
+      max = numbers[0];
+	  for (double x : numbers)
+	  {
+		if (x > max)
+		{
+			max = x;
+		}
+		else if (x < min)
+		{
+			min = x;
+		}
+	  }
+    }
+
+  void svg_begin(double width, double height)
+  {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+  }
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
+
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke, string fill)
+{
+  cout<<"<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+}
+
