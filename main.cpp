2f440510875a524c2539f602f597ebe93fcf7bb1
Dima (ShepelevDD) <shepelev.mit@yandex.ru>
2020-04-10 19:18:43 +0300

    Итоговый код

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "lab03.h"
 using namespace std;
 
 vector<double>
@@ -12,22 +13,6 @@ input_numbers(size_t count)
     return result;
   }
 
-void find_minmax(vector<double> numbers, double& min, double& max)
-    {
-      min = numbers[0];
-      max = numbers[0];
-	  for (double x : numbers)
-	  {
-		if (x > max)
-		{
-			max = x;
-		}
-		else if (x < min)
-		{
-			min = x;
-		}
-	  }
-    }
 vector <size_t> make_histogram(const vector<double>& numbers, size_t bin_count)
     {
         double min;
@@ -77,26 +62,9 @@ void show_histogram_text(vector<size_t> bins)
 }
 }
 
-void
-svg_begin(double width, double height) {
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
 
-void
-svg_end() {
-    cout << "</svg>\n";
-}
 
-void
-svg_text(double left, double baseline, string text) {
-    cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
 
-}
 void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
 {
   cout<<"<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
