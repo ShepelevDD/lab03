57153f5b23da9965eacfad051fc307c3e4c5bdb1
Dmitry (ShepelevDD) <shepelev.mit@yadnex.ru>
2020-05-08 11:22:06 +0300

    main.cpp функция input_numbers

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 vector<double>
-input_numbers(size_t count)
+input_numbers(istream& in, size_t count)
   {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
@@ -66,6 +66,7 @@ void show_histogram_text(vector<size_t> bins)
 
 void
 show_histogram_svg(const vector<size_t>& bins, size_t number_count) {
+const auto IMAGE_WIDTH = 400;
 const auto IMAGE_HEIGHT = 300;
 const auto TEXT_LEFT = 20;
 const auto TEXT_BASELINE = 20;
@@ -73,16 +74,7 @@ const auto TEXT_WIDTH = 50;
 const auto BIN_HEIGHT = 30;
 const auto BLOCK_WIDTH = 10;
 double top = 0;
-size_t zadanie;
-cerr<<"image width: ";
-cin >> zadanie;
-	while (zadanie > 800 || zadanie < 70 || zadanie < (number_count * BLOCK_WIDTH/ 3))
-	{
-		cout << "Ooops! Try again: Width mustnt be less than 70, bigger than 800 or less than 1/3 of number count * 10. Enter image width: ";
-		cin >> zadanie;
-	}
-const auto IMAGE_WIDTH = zadanie;
-svg_begin(zadanie, 300);
+svg_begin(400, 300);
 for (size_t bin : bins)
     {
     const double bin_width = BLOCK_WIDTH * bin;
@@ -103,7 +95,7 @@ int main()
 	double min, max;
 	cerr << "Enter number count ";
 	cin >> number_count;
-	const auto numbers = input_numbers(number_count);
+	const auto numbers = input_numbers(cin, number_count);
 	size_t bin_count;
 	cerr << "Enter bucket count" << endl;
 	cin >> bin_count;
