26ea4278b6b84e6546da1c3d4b827ccf03a9c1b2
Dmitry (ShepelevDD) <shepelev.mit@yadnex.ru>
2020-05-08 17:17:04 +0300

    main.cpp итог

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "lab03.h"
 #include "svg.h"
 #include <string>
+#include <sstream>
 using namespace std;
 
 
@@ -113,33 +114,48 @@ for (size_t bin : bins)
     svg_end();
 }
 
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    // TODO: дописывать данные к буферу.
+    size_t data_size = item_size * item_count;
+     stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<const char*>(items), data_size);
+    return 0;
+}
+
+Input
+download(const string& address) {
+    stringstream buffer;
+    curl_global_init(CURL_GLOBAL_ALL);
+       CURL *curl = curl_easy_init();
+             if(curl) {
+                        CURLcode res;
+                        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+                        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+                        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+                        res = curl_easy_perform(curl);
+                        if (res!=CURLE_OK)
+                        {
+                         cout<<curl_easy_strerror(res);
+                         exit(1);
+                        }
+                        curl_easy_cleanup(curl);
+                      };
+
+    return read_input(buffer, false);
+}
 
 int
 main(int argc, char* argv[])
-{   curl_global_init(CURL_GLOBAL_ALL);
-    for (size_t i=0; i<argc; i++)
-   {
-    if(argc>1)
-    {
-        CURL *curl = curl_easy_init();
-             if(curl) {
-                        CURLcode res;
-                        curl_easy_setopt(curl, CURLOPT_URL, argv[i]);
-                        res = curl_easy_perform(curl);
-                        curl_easy_cleanup(curl);
-                        if (res!=0)
-                        {
-                         string curl_error = curl_easy_strerror(res);
-                         cout<<curl_error;
-                         exit(1);
-                        }
-                      };
+{
+    Input input;
+    if (argc > 1) {
+        input = download(argv[1]);
+    } else {
+        input = read_input(cin, true);
     }
-    else{
-    const auto input = read_input(cin, true);
+
     const auto bins = make_histogram(input);
-    show_histogram_svg (bins);
-    }
-   }
+    show_histogram_svg(bins);
 	return 0;
 }
