7e0cd91b29d0090bf1c1d602694c9be80d8c7f93
Dmitry (ShepelevDD) <shepelev.mit@yadnex.ru>
2020-05-22 17:44:09 +0300

    svg.cpp итоговый вариант

--- svg.cpp
+++ svg.cpp
@@ -1,6 +1,8 @@
 #include "lab03.h"
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include <sstream>
 using namespace std;
 
  void svg_begin(double width, double height)
@@ -18,10 +20,43 @@ void svg_end() {
 
 void svg_text(double left, double baseline, string text) {
     cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
-
 }
 
 void svg_rect(double x, double y, double width, double height, string stroke, string fill)
 {
   cout<<"<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+}
+
+string make_info_text(){
+    stringstream buffer;
+    DWORD dwVersion = 0;
+    dwVersion = GetVersion();
+    DWORD info = GetVersion();
+    DWORD mask = 0x0000ffff;
+    DWORD version = info & mask;
+    printf("Windows decimal-version is %u\n", version);
+    printf("Windows 16x version : %x\n", version);
+    if ((info & 0x80000000) == 0) {
+    DWORD platform = info >>16;
+    DWORD mask_major = 0x000000ff;
+    DWORD version_major = version & mask_major;
+    DWORD version_minor = version >> 8;
+    DWORD build = platform;
+    printf("Windows major version is : %x\n", version_major);
+    printf("Windows minor version is : %x\n", version_minor);
+     printf("Version is %d.%d (%d)\n",
+               version_major,
+               version_minor,
+                build);
+  char system_dir[MAX_PATH];
+  char comp_name [MAX_COMPUTERNAME_LENGTH+1];
+  DWORD size = sizeof(comp_name);
+  GetComputerNameA(comp_name, &size);
+
+  buffer <<"Computer name:"<< comp_name;
+}
+
+    // TODO: получить версию системы, записать в буфер.
+    // TODO: получить имя компьютера, записать в буфер.
+    return buffer.str();
 }
