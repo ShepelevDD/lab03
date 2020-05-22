0cb61ffaa8b9f52d992a3d5d3743f7a82b046afa
Dmitry (ShepelevDD) <shepelev.mit@yadnex.ru>
2020-05-22 13:22:46 +0300

    main.cpp добавил printf()

--- main.cpp
+++ main.cpp
@@ -67,6 +67,7 @@ void show_histogram_text(vector<size_t> bins)
 
 void
 show_histogram_svg(const vector<size_t>& bins, size_t number_count) {
+const auto IMAGE_WIDTH = 400;
 const auto IMAGE_HEIGHT = 300;
 const auto TEXT_LEFT = 20;
 const auto TEXT_BASELINE = 20;
@@ -74,16 +75,7 @@ const auto TEXT_WIDTH = 50;
 const auto BIN_HEIGHT = 30;
 const auto BLOCK_WIDTH = 10;
 double top = 0;
-size_t zadanie;
-cerr<<"image width: ";
-cin >> zadanie;
-	while (zadanie > 800 || zadanie < 70 || zadanie < (number_count * BLOCK_WIDTH/ 3))
-	{
-		cout << "Ooops! Try again: Width mustnt be less than 70, bigger than 800 or less than 1/3 of number count * 10. Enter image width: ";
-		cin >> zadanie;
-	}
-const auto IMAGE_WIDTH = zadanie;
-svg_begin(zadanie, 300);
+svg_begin(400, 300);
 for (size_t bin : bins)
     {
     const double bin_width = BLOCK_WIDTH * bin;
@@ -99,8 +91,7 @@ for (size_t bin : bins)
 
 int main()
 {
-	const size_t SCREEN_WIDTH = 80;
-	const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+printf("n = %08x\n", 0x1234567);
 	size_t number_count;
 	double min, max;
 	cerr << "Enter number count ";
