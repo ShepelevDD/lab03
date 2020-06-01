e570069ce419cf6db55574a1049e122b8ac1c96c
Dmitry (ShepelevDD) <shepelev.mit@yadnex.ru>
2020-06-01 22:41:16 +0300

    защита ЛР3 (итог)

--- test.cpp
+++ test.cpp
@@ -36,6 +36,14 @@ void test_single(){
     assert(max == 3);
 }
 
+
+void test_width() {
+    assert(custome_width(50, 10, 10) == "Ooops! Width < 70. Enter the width again:");
+    assert(custome_width(850, 10, 10) == "Ooops! Width > 800. Enter the width again:");
+    assert(custome_width(75, 24, 10) == "Ooops! Width < BLOCK_WIDTH*number_count/3. Enter the width again:");
+
+}
+
 void test_nothing(){
     double min = 0;
     double max = 0;
@@ -43,5 +51,6 @@ void test_nothing(){
     assert(min == 3);
     assert(max == 3);
 }
+
 
 
