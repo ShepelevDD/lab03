3b023e47fc298db4126e9cb992b8ec117f52a609
Dmitry (ShepelevDD) <shepelev.mit@yadnex.ru>
2020-06-02 20:28:02 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -15,53 +15,50 @@ input_numbers(size_t count)
     return result;
   }
 
-vector <size_t> make_histogram(const vector<double>& numbers, size_t bin_count)
-    {
-        double min;
-        double max;
-        find_minmax(numbers,min,max);
-        vector<size_t>bins(bin_count, 0);
-        for (double x : numbers)
-	  {
-		size_t bin_index = ((x - min) / (max - min)) * bin_count;
-		if (bin_index == bin_count)
-		{
-			bin_index--;
-		}
-		bins[bin_index]++;
-      }
-      return bins;
-    }
 
-void show_histogram_text(vector<size_t> bins)
-{
-    const size_t SCREEN_WIDTH = 80;
-	const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-	size_t maxbin = 0;
-	for (size_t bin : bins) {
-		if (bin > maxbin) {
-			maxbin = bin;
-		}
-	}
-    double factor = MAX_ASTERISK/maxbin;
-	for (size_t bin : bins)
-	{
-		if (bin < 100)
-		{
-			cout << " ";
-			if (bin < 10)
-			{
-				cout << " ";
-			}
-		}
-	cout << bin << '|';
-	size_t height = bin * factor;
-	for (int i = 0; i < height; i++)
-	{
-		cout << '*';
-	}
-	cout << endl;
+
+vector<size_t> make_histogram(const vector<double>& numbers, const size_t count) {
+    vector<size_t> result(count);
+    double min;
+    double max;
+    find_minmax(numbers, min, max);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * count);
+        if (bin == count) {
+            bin--;
+        }
+        result[bin]++;
+    }
+    return result;
 }
+void show_histogram_text(vector<size_t> bins) {
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+    for (size_t bin : bins) {
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin << "|";
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
 }
 
 
